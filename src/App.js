0cc39640eccf8ee18f36c4bce3fe60615cd2fc4b
sevinc995 <sevincseferova25@gmail.com>
2025-08-24 15:45:55 +0000

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { useState } from "react";
 
 function App() {
+  const [a, seta] = useState(0)
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <h1>{a}</h1>
+      <button onClick={() => seta((t) => t+1)}> + </button>
+      <button onClick={() => seta((t) => t-1)}> - </button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './style.css';
+import './style.css';
 import App from './App';
+import Home from './home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Home/>
   </React.StrictMode>
 );
 
